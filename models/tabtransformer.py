fixed preserve gradient for TabTransformer

models/tabtransformer.py
@@ -150,29 +150,42 @@ class TabTransformer(BaseModelTorch):
         self.load_model(filename_extension="best", directory="tmp")
         return loss_history, val_loss_history
 
-    def predict_helper(self, X):
+    def predict_helper(self, X, keep_grad=False):
         self.model.eval()
-        X = np.array(X, dtype=np.float)
-        X = torch.tensor(X).float()
 
-        test_dataset = TensorDataset(X)
-        test_loader = DataLoader(dataset=test_dataset, batch_size=self.args.val_batch_size, shuffle=False,
-                                 num_workers=2)
+        if keep_grad:
+            x_categ = X[:, self.args.cat_idx].int().to(self.device) if self.args.cat_idx else None
+            x_cont = X[:, self.num_idx].to(self.device)
 
-        predictions = []
+            preds = self.model(x_categ, x_cont)
 
-        with torch.no_grad():
-            for batch_X in test_loader:
-                x_categ = batch_X[0][:, self.args.cat_idx].int().to(self.device) if self.args.cat_idx else None
-                x_cont = batch_X[0][:, self.num_idx].to(self.device)
+            if self.args.objective == "binary":
+                preds = torch.sigmoid(preds)
 
-                preds = self.model(x_categ, x_cont)
+            return preds
+        else:
 
-                if self.args.objective == "binary":
-                    preds = torch.sigmoid(preds)
+            X = np.array(X, dtype=np.float)
+            X = torch.tensor(X).float()
 
-                predictions.append(preds.cpu())
-        return np.concatenate(predictions)
+            test_dataset = TensorDataset(X)
+            test_loader = DataLoader(dataset=test_dataset, batch_size=self.args.val_batch_size, shuffle=False,
+                                     num_workers=2)
+
+            predictions = []
+
+            with torch.no_grad():
+                for batch_X in test_loader:
+                    x_categ = batch_X[0][:, self.args.cat_idx].int().to(self.device) if self.args.cat_idx else None
+                    x_cont = batch_X[0][:, self.num_idx].to(self.device)
+
+                    preds = self.model(x_categ, x_cont)
+
+                    if self.args.objective == "binary":
+                        preds = torch.sigmoid(preds)
+
+                    predictions.append(preds.cpu())
+            return np.concatenate(predictions)
 
     @classmethod
     def define_trial_parameters(cls, trial, args):