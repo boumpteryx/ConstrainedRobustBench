022305190a16f79e6ac54b5a8f148e09d6fe21f7
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-02-15 08:12:59 +0900

    Added Precision jobs and API

---
+++ train_bis.sh
@@ -0,0 +1,16 @@
+# URL
+python train.py --config config/url.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+python train.py --config config/url.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+python train.py --config config/url.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+
+# WIDS
+python train.py --config config/wids.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
+python train.py --config config/wids.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
+python train.py --config config/wids.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
+
+# CTU
+python train.py --config config/ctu_13_neris.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+python train.py --config config/ctu_13_neris.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+python train.py --config config/ctu_13_neris.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+
+
