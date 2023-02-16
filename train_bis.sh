47a8e9048b160e10651cc84746140318eb441bf0
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-02-16 15:25:20 +0900

    Updated config to use multi-gpu training

--- train_bis.sh
+++ train_bis.sh
@@ -1,16 +1,19 @@
 # URL
-python train.py --config config/url.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
-python train.py --config config/url.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
-python train.py --config config/url.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+python train.py --config config/url.yml --model_name DeepFM --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/url.yml --model_name VIME --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/url.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/url.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+
 
 # WIDS
-python train.py --config config/wids.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/wids.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/wids.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
+python train.py --config config/wids.yml --model_name DeepFM --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/wids.yml --model_name VIME --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/wids.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/wids.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+
 
 # CTU
-python train.py --config config/ctu_13_neris.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
-python train.py --config config/ctu_13_neris.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
-python train.py --config config/ctu_13_neris.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
-
-
+python train.py --config config/ctu_13_neris.yml --model_name DeepFM --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/ctu_13_neris.yml --model_name VIME --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/ctu_13_neris.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
+#python train.py --config config/ctu_13_neris.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 1024 --val_batch_size 2048
--- trainall.sh
+++ trainall.sh
@@ -1,30 +1,12 @@
 # LCLD
-python train.py --config config/lcld_v2_time.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 512 --val_batch_size 1024
-python train.py --config config/lcld_v2_time.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"  --batch_size 512 --val_batch_size 1024
-python train.py --config config/lcld_v2_time.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"  --batch_size 512 --val_batch_size 1024
-python train.py --config config/lcld_v2_time.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"  --batch_size 512 --val_batch_size 1024
+#python train.py --config config/lcld_v2_time.yml --model_name DeepFM --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230" --batch_size 512 --val_batch_size 1024
+python train.py --config config/lcld_v2_time.yml --model_name VIME --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"  --batch_size 512 --val_batch_size 1024
+#python train.py --config config/lcld_v2_time.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"  --batch_size 512 --val_batch_size 1024
+#python train.py --config config/lcld_v2_time.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"  --batch_size 512 --val_batch_size 1024
 
 # Malware
-python train.py --config config/malware.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/malware.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/malware.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/malware.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/malware.yml --model_name ModelTree --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-
-# URL
-python train.py --config config/url.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/url.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/url.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-
-# WIDS
-python train.py --config config/wids.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/wids.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/wids.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-
-
-# CTU
-python train.py --config config/ctu_13_neris.yml --model_name DeepFM --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/ctu_13_neris.yml --model_name VIME --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-python train.py --config config/ctu_13_neris.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "yamizi" --api_key "tJX8KSgUh11CrvELlxhNht230"
-
-
+#python train.py --config config/malware.yml --model_name DeepFM --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"
+#python train.py --config config/malware.yml --model_name VIME --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"
+#python train.py --config config/malware.yml --model_name TabTransformer --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"
+python train.py --config config/malware.yml --model_name TORCHRLN --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"
+#python train.py --config config/malware.yml --model_name ModelTree --optimize_hyperparameters  --workspace "constrained-robustbench" --api_key "tJX8KSgUh11CrvELlxhNht230"
